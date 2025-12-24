ad5d4ca76cfaf3d05cabf40b512dd5bc80cb1701
QuangIcetea <quangnguyenngoc314@gmail.com>
2025-12-24 16:59:55 +0700

    fix: remove trailing slash & change route ws to /ws in order to match nginx conf

--- notification-service/internal/handlers/handler.go
+++ notification-service/internal/handlers/handler.go
@@ -71,7 +71,7 @@ func New(cfg *Config) (http.Handler, error) {
 
 	// WebSocket endpoint for real-time notifications
 	groupWebSocket := NewGroupWebSocket(cfg.Container)
-	r.GET("/ws/v1/notifications/", groupWebSocket.WebsocketHandleConnection)
+	r.GET("/ws/v1/notifications", groupWebSocket.WebsocketHandleConnection)
 
 	return r, nil
 }
